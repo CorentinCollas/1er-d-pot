36bd8d8a1a5c38be5b92d389a046865566e1905b
CorentinCollas <101388959+CorentinCollas@users.noreply.github.com>
2022-03-11 09:45:59 +0100

    Update Operations.cpp

--- Operations.cpp
+++ Operations.cpp
@@ -4,12 +4,30 @@ int addition(int a, int b)
 {
 	int resultat;
 	
-	resultat = a * b;
+	resultat = a + b;
 	
 	return resultat;
 }
 
 int soustraction(int a, int b)
+{
+	int resultat;
+	
+	resultat = a - b;
+	
+	return resultat;
+}
+
+int multiplication(int a, int b)
+{
+	int resultat;
+	
+	resultat = a * b;
+	
+	return resultat;
+}
+
+int division(int a, int b)
 {
 	int resultat;
 	
